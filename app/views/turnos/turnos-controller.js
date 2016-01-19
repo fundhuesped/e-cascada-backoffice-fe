afbd120a9118cdba9bd3a291ba4aa422d5cfb99c
Nicolas Lelio Gonzalez <nicolas.lelio.gonzalez@gmail.com>
2016-01-19 02:29:32 -0300

    #HUES-120 Added Medicos events to calendar

--- app/views/turnos/turnos-controller.js
+++ app/views/turnos/turnos-controller.js
@@ -2,18 +2,44 @@
     'use strict';
     
     function turnosCtrl (TurnosSrv) {
+	    var date = new Date();
+    	var d = date.getDate();
+    	var m = date.getMonth();
+    	var y = date.getFullYear();
     	 /* config object */
-    this.eventSources = [TurnosSrv.events];
+    this.eventSources = [];
 	    this.calendarConfig = 
 	      {
 	        height: 450,
-	        editable: true,
+	        editable: false,
+	        lang: 'es',
+	        weekends: false,
 	        header:{
 	          left: 'month agendaWeek agendaDay',
 	          center: 'title',
 	          right: 'today prev,next'
 	        },
 	    };
+	this.toggleSources = function toggleSources(){
+		this.eventSources.length = 0;
+		if(this.ramirez){
+			var ramirezCal = 	     [ 
+				{title: 'Hanna Blade', start: new Date(y, m, d + 3, 9, 0),end: new Date(y, m, d + 3, 16, 20),allDay: false},
+				{title: 'Kenneth Burts', start: new Date(y, m, d + 3, 9, 20),end: new Date(y, m, d + 3, 16, 40),allDay: false},
+	      		{title: 'Vacaciones',start: new Date(y, m, d + 5),end: new Date(y, m, d + 20)}];
+			this.eventSources.push(ramirezCal);
+		}
+		if(this.fernandez){
+			var fernandezCal = 	     [ 
+				{title: 'Jessie Blake', start: new Date(y, m, d + 3, 9, 0),end: new Date(y, m, d + 3, 9, 20),allDay: false,color:"#ee8505"},
+				{title: 'Tom Umland', start: new Date(y, m, d + 3, 9, 20),end: new Date(y, m, d + 3, 9, 40),allDay: false,color:"#ee8505"},
+				{title: 'Molly Truluck', start: new Date(y, m, d + 3, 9, 40),end: new Date(y, m, d + 3, 10, 0),allDay: false,color:"#ee8505"},
+				{title: 'Merrilee Eoff', start: new Date(y, m, d + 3, 10, 0),end: new Date(y, m, d + 3, 10, 20),allDay: false,color:"#ee8505"}];
+			this.eventSources.push(fernandezCal);
+		}
+		console.log(this.eventSources);
+	}
+
     }
     angular.module('turnos.turnos').controller('TurnosCtrl',['TurnosSrv',turnosCtrl]);
 })();
