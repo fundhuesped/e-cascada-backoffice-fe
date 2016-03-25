725c69383ca1d9012d7a17664e0e6a388ae98064
Ruben Ghio <rubenghio@gmail.com>
2016-03-25 01:56:26 -0300

    Creacion branch para heroku

--- app/resources/resources-module.js
+++ app/resources/resources-module.js
@@ -1,6 +1,6 @@
 (function(){
  'use strict';
  angular.module('turnos.resources',[])
-    //.value('apiBase', 'http://localhost:8000/') // Mock
-    .value('apiBase', 'http://huesapi.herokuapp.com/'); // Heroku
+    .value('apiBase', 'http://localhost:8000/') // Mock
+    //.value('apiBase', 'http://huesapi.herokuapp.com/'); // Heroku
 })();
