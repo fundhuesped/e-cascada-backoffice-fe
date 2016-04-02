fa6245b3b70deb66aa16ea088f8b0fd8c6af8427
Ruben Ghio <rubenghio@gmail.com>
2016-04-02 18:05:37 -0300

    Correccion branch

--- app/resources/resources-module.js
+++ app/resources/resources-module.js
@@ -1,5 +1,5 @@
 (function(){
  'use strict';
  angular.module('turnos.resources',[])
-    .value('apiBase', 'http://localhost:8000/') // Mock
+	.value('apiBase', 'http://huesapi.herokuapp.com/'); // Heroku
 })();
