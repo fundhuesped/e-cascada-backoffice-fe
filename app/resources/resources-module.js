de13e604403bd3adee7d5e178cb5ef38e2cfc599
Ruben Ghio <rubenghio@gmail.com>
2016-05-07 21:22:12 +0000

    Actualizacion de variables de ambiente azure

--- app/resources/resources-module.js
+++ app/resources/resources-module.js
@@ -1,5 +1,5 @@
 (function(){
  'use strict';
  angular.module('turnos.resources',[])
-	.value('apiBase', 'http://huesapi.herokuapp.com/'); // Heroku
+	.value('apiBase', 'http://huespedserver.eastus.cloudapp.azure.com:8000/'); // Azure
 })();
